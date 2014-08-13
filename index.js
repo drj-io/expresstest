0a8b2daeead69ca44d4b778d1692d8518a2547cd
David <dub@zenodub.com>
2014-08-13 12:54:54 -0600

    username and password is working

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var users = 
+    [
+        {
+            username: 'david',
+            password: 'qwerty'
+        },
+        {
+            username: 'joe',
+            password: 'password'
+        }
+    ];
+
+
+
+
+
 
 
 app.set('view engine', 'ejs');
 
-
-
-app.get('/', function(req, res) {
+app.get('/user', function(req, res) {
   res.render('login', { title: 'The index page!', body: "" })
 });
 
+app.post('/user/login',function(req,res){
+    console.log( req.body.username, req.body.password);
 
 
+    var validUser = false;
 
+    users.forEach(function(user ,index){
+        console.log(user.username);
+
+        if((user.username == req.body.username) && (user.password == req.body.password)){
+            console.log('found a valid user');
+            validUser = user.username;
+
+
+        }
+    });
+    
+    if(validUser){
+        res.send('you are authenticated!')
+    }
+    else{
+        res.send('incorrect username and password!')
+    }
+
+
+});
 
 
 
